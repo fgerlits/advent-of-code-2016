7b9e1f95418d4240888078cbc1d7850a8c4c36be
Ferenc Gerlits <fgerlits@gmail.com>
2016-12-05 22:25:17 +0100

    Move the reusable functions to a separate file

--- 5/one.cpp
+++ 5/one.cpp
@@ -1,48 +1,6 @@
 // compile with: g++ --std=c++11 -O3 ./solve.cpp -lcrypto
 
-#include <openssl/md5.h>
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <iomanip>
-
-std::string createMd5(const std::string & input) {
-  unsigned char digest[MD5_DIGEST_LENGTH];
-  MD5((unsigned char *)input.c_str(), input.size(), (unsigned char *)&digest);
-  return std::string((const char *)&digest, MD5_DIGEST_LENGTH);
-}
-
-size_t countLeadingZeros(const std::string & input) {
-  size_t numZeros = 0;
-  size_t i = 0;
-  while (i < input.size() && input[i] == '\0') {
-    numZeros += 2;
-    ++i;
-  }
-  if (i < input.size() && (unsigned char)(input[i]) < '\020') {
-    numZeros += 1;
-  }
-  return numZeros;
-}
-
-std::string format(const std::string & input) {
-  std::ostringstream ss;
-  for (unsigned char c : input) {
-    ss << std::hex << std::setw(2) << std::setfill('0') << (int)(c);
-  }
-  return ss.str();
-}
-
-char nextSolutionChar(const std::string & input, int & n) {
-  size_t numZeros = 0;
-  do {
-    std::string md5 = createMd5(input + std::to_string(n));
-    numZeros = countLeadingZeros(md5);
-    ++n;
-  } while (numZeros < 5);
-  std::string hash = format(createMd5(input + std::to_string(n - 1)));
-  return hash[5];
-}
+#include "one.h"
 
 int main(int argc, char * argv[])
 {
