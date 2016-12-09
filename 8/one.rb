608ab998a63e66609411f70ada5e0fe12683421a
Ferenc Gerlits <fgerlits@gmail.com>
2016-12-09 07:55:42 +0100

    Refactoring: use a lambda instead of an enum

--- 8/one.rb
+++ 8/one.rb
@@ -36,29 +36,22 @@ class Instruction
     def initialize(line)
         case line
             when /rect (\d+)x(\d+)/
-                @instr = :rect
-                @x = $1.to_i
-                @y = $2.to_i
+                x = $1.to_i
+                y = $2.to_i
+                @action = lambda {|_| _.rect(x, y)}
             when /rotate column x=(\d+) by (-?\d+)/
-                @instr = :rotate_column
-                @column = $1.to_i
-                @by = $2.to_i
+                x = $1.to_i
+                by = $2.to_i
+                @action = lambda {|_| _.rotate_column(x, by)}
             when /rotate row y=(\d+) by (-?\d+)/
-                @instr = :rotate_row
-                @row = $1.to_i
-                @by = $2.to_i
+                y = $1.to_i
+                by = $2.to_i
+                @action = lambda {|_| _.rotate_row(y, by)}
         end
     end
 
     def apply(tiny_screen)
-        case @instr
-            when :rect
-                tiny_screen.rect(@x, @y)
-            when :rotate_column
-                tiny_screen.rotate_column(@column, @by)
-            when :rotate_row
-                tiny_screen.rotate_row(@row, @by)
-        end
+        @action.call(tiny_screen)
     end
 end
 
