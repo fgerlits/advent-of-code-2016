7857f9787cf6900db035b38f594f403797ede2a3
Ferenc Gerlits <fgerlits@gmail.com>
2016-12-10 13:09:41 +0100

    Fix whitespace

--- 10/two.rb
+++ 10/two.rb
@@ -5,5 +5,5 @@ require_relative 'one'
 if __FILE__ == $0
     state = Bots.new(ARGF.readlines) 
     state.take_all_steps
-	puts state.output.values_at(0, 1, 2).flatten.inject(:*)
+    puts state.output.values_at(0, 1, 2).flatten.inject(:*)
 end
