2814856fd99b1b5c4b295e4056acd027ee411b84
Ferenc Gerlits <fgerlits@gmail.com>
2016-12-18 09:10:53 +0100

    Puzzles for day 18
    This is slightly uglier, but slightly faster: on 40,000 rows
    it takes 34s instead of 48s.

--- 18/one.rb
+++ 18/one.rb
@@ -5,23 +5,27 @@ def trap?(x, y, z)
 end
 
 def next_row(row)
-    ('.' + row + '.').each_char.each_cons(3).map do |x, y, z|
-        trap?(x, y, z) ? '^' : '.'
-    end.join
+    ([false] + row + [false]).each_cons(3).map do |x, y, z|
+        trap?(x, y, z)
+    end
 end
 
 def count_safe_tiles(num_rows, first_row)
     num_safe_tiles = 0
     row = first_row
     num_rows.times do
-        num_safe_tiles += row.count('.')
+        num_safe_tiles += row.count(false)
         row = next_row(row)
     end
     num_safe_tiles
 end
 
+def process_input(input)
+    input.each_char.map{|c| c == '^'}
+end
+
 if __FILE__ == $0
     num_rows = ARGV.shift.to_i
-    first_row = ARGF.read.chomp
+    first_row = process_input(ARGF.read.chomp)
     puts count_safe_tiles(num_rows, first_row)
 end
--- 18/test_one.rb
+++ 18/test_one.rb
@@ -5,23 +5,29 @@ require 'test/unit'
 
 class TestOne < Test::Unit::TestCase
     def test_trap?
-        assert trap?('^', '^', '.')
-        assert trap?('.', '^', '^')
-        assert trap?('.', '.', '^')
-        assert trap?('^', '.', '.')
-        assert ! trap?('.', '.', '.')
-        assert ! trap?('.', '^', '.')
-        assert ! trap?('^', '.', '^')
-        assert ! trap?('^', '^', '^')
+        assert trap?(true, true, false)
+        assert trap?(false, true, true)
+        assert trap?(false, false, true)
+        assert trap?(true, false, false)
+        assert ! trap?(false, false, false)
+        assert ! trap?(false, true, false)
+        assert ! trap?(true, false, true)
+        assert ! trap?(true, true, true)
     end
 
     def test_next_row
-        assert_equal '.^^^^', next_row('..^^.')
-        assert_equal '^^..^', next_row('.^^^^')
+        assert_equal process_input('.^^^^'),
+                     next_row(process_input('..^^.'))
+        assert_equal process_input('^^..^'),
+                     next_row(process_input('.^^^^'))
     end
 
     def test_count_traps
-        assert_equal 6, count_safe_tiles(3, '..^^.')
-        assert_equal 38, count_safe_tiles(10, '.^^.^.^^^^')
+        assert_equal 6, count_safe_tiles(3, process_input('..^^.'))
+        assert_equal 38, count_safe_tiles(10, process_input('.^^.^.^^^^'))
+    end
+
+    def test_process_input
+        assert_equal [false, true, true], process_input('.^^')
     end
 end
